c86d364b32472a3e034137bd4cbe2f7f4f721c7d
Paul Legato <pjlegato@gmail.com>
2010-07-09 16:05:08 -0700

    New project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-options "1.0.0-SNAPSHOT"
+  :description "Options pricing library"
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]])
---
+++ test/clojure_options/core_test.clj
@@ -0,0 +1,6 @@
+(ns clojure-options.core-test
+  (:use [clojure-options.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false))
